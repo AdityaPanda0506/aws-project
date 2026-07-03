dd2b6ee8ed7baf8c6aba223ce5bf3b9be4e914e3
Aditya Panda <aadityapanda5@gmail.com>
2026-07-03 14:18:04 +0530

    Configure production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,24 +2,20 @@ import axios from "axios";
 
 const api = axios.create({
 
-    baseURL: "http://127.0.0.1:8000"
+    baseURL: import.meta.env.VITE_API_URL
 
 });
 
 export const uploadCSV = (formData) =>
-
     api.post("/upload/", formData);
 
 export const getHistory = () =>
-
     api.get("/history");
 
 export const getDashboard = () =>
-
     api.get("/dashboard");
 
 export const getReport = (id) =>
-
     api.get(`/report/${id}`);
 
 export default api;
